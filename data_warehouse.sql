cbef140591bfbe6ee6b48e5c2fc902f5b2032cde
Georgius <georgiusbenedikt09@gmail.com>
2026-02-23 22:10:05 +0000

    Create sql file to initialise the database

---
+++ data_warehouse.sql
@@ -0,0 +1,27 @@
+USE master;
+
+GO
+
+
+-- Create a new database named DataWarehouse
+CREATE DATABASE DataWarehouse;
+
+GO
+
+USE DataWarehouse;
+
+GO
+
+-- Create three schemas named bronze, silver, and gold
+CREATE SCHEMA bronze;
+
+GO
+
+CREATE SCHEMA silver;
+
+GO
+
+CREATE SCHEMA gold;
+
+GO
+
